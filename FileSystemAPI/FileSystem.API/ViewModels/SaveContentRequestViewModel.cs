9d5819287f059f6adf47422783524a14b240ad8d
Ivan Perički <zagorec92@users.noreply.github.com>
2022-11-05 12:57:55 +0100

    Added documentation for Save/Update ContentRequestViewModels

--- FileSystemAPI/FileSystem.API/ViewModels/SaveContentRequestViewModel.cs
+++ FileSystemAPI/FileSystem.API/ViewModels/SaveContentRequestViewModel.cs
@@ -1,22 +1,48 @@
-﻿using FileSystem.Core.Enums;
+﻿using FileSystem.Controllers;
+using FileSystem.Core.Enums;
 using FileSystem.Core.Models.Requests;
 using FileSystem.Infrastructure;
 
 namespace FileSystem.API.ViewModels
 {
+	/// <summary>
+	/// Type that encapsulates the <see cref="HttpRequest.Body"/> parameters.
+	/// </summary>
+	/// <remarks>
+	/// Used in <see cref="ContentController.Save(Guid, SaveContentRequestViewModel)"/> action.
+	/// </remarks>
 	public class SaveContentRequestViewModel
 	{
+		/// <summary>
+		/// The <see cref="Guid"/> content parent id.
+		/// </summary>
 		public Guid? ParentId { get; set; }
+		
+		/// <summary>
+		/// The <see cref="string"/> content name.
+		/// </summary>
 		public string Name { get; set; }
+
+		/// <summary>
+		/// The <see cref="ContentType"/> content type.
+		/// </summary>
 		public ContentType Type { get; set; }
 
+		/// <summary>
+		/// Creates a new <see cref="SaveContentRequestViewModel"/> instance.
+		/// </summary>
 		public SaveContentRequestViewModel()
 		{
 			Name = Constants.RootDirectory;
 			Type = (byte)ContentType.Directory;
 		}
 
+		/// <summary>
+		/// Creates a new <see cref="SaveContentRequest"/> instance from the current one.
+		/// </summary>
+		/// <param name="customerId">The <see cref="Guid"/> customer identifier.</param>
+		/// <returns>The <see cref="SaveContentRequest"/> instance.</returns>
 		public SaveContentRequest ToSaveContentRequest(Guid customerId)
-			=> new(customerId, ParentId, Name, (ContentType)Type);
+			=> new(customerId, ParentId, Name, Type);
 	}
 }
--- FileSystemAPI/FileSystem.API/ViewModels/UpdateContentRequestViewModel.cs
+++ FileSystemAPI/FileSystem.API/ViewModels/UpdateContentRequestViewModel.cs
@@ -1,12 +1,32 @@
-﻿using FileSystem.Core.Models.Requests;
+﻿using FileSystem.Controllers;
+using FileSystem.Core.Models.Requests;
 
 namespace FileSystem.API.ViewModels
 {
+	/// <summary>
+	/// Type that encapsulates the <see cref="HttpRequest.Body"/> parameters.
+	/// </summary>
+	/// <remarks>
+	/// Used in <see cref="ContentController.Update(Guid, Guid, UpdateContentRequestViewModel)"/> action.
+	/// </remarks>
 	public class UpdateContentRequestViewModel
 	{
+		/// <summary>
+		/// The <see cref="Guid"/> content parent id.
+		/// </summary>
 		public Guid ParentId { get; set; }
+
+		/// <summary>
+		/// The <see cref="string"/> content name.
+		/// </summary>
 		public string Name { get; set; }
 
+		/// <summary>
+		/// Creates a new <see cref="UpdateContentRequest"/> instance from the current one.
+		/// </summary>
+		/// <param name="customerId">The <see cref="Guid"/> customer identifier.</param>
+		/// <param name="id">The <see cref="Guid"/> content identifier.</param>
+		/// <returns>The <see cref="UpdateContentRequest"/> instance.</returns>
 		public UpdateContentRequest ToUpdateContentRequest(Guid customerId, Guid id)
 			=> new(customerId, id, ParentId, Name);
 	}
